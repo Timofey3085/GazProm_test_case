e7fad84dfe25aa0f1471749267bf187802563385
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-05-18 23:21:58 +0300

    Next Solution_03

--- task_02.py
+++ task_02.py
@@ -9,6 +9,7 @@ m = [{11, 3, 5}, {2, 17, 87, 32}, {4, 44}, {24, 11, 9, 7, 8}]
 4 собрать все множества в один кортеж
 *написать решения в одну строку
 """
+import unittest
 from itertools import chain
 
 import pytest
@@ -52,9 +53,14 @@ def test_average_value():
     assert average_value([{11, 3, 5}, {2, 17, 87, 32}, {4, 44}, {24, 11, 9, 7, 8}]) == 18.857142857142858
 
 
-def test_collect_to_tuple():
-    """Pytest"""
-    assert collect_to_tuple([{11, 3, 5}, {2, 17, 87, 32}, {4, 44}, {24, 11, 9, 7, 8}]) == (11, 3, 5, 32, 17, 2, 87, 4, 44, 7, 8, 9, 11, 24)
+class TestCollectToTuple(unittest.TestCase):
+
+    def test_collect_to_tuple(self):
+        """Unittest"""
+        self.assertEqual(collect_to_tuple(
+            [{11, 3, 5}, {2, 17, 87, 32}, {4, 44}, {24, 11, 9, 7, 8}]),
+            (11, 3, 5, 32, 17, 2, 87, 4, 44, 7, 8, 9, 11, 24)
+        )
 
 
 if __name__ == '__main__':
@@ -63,3 +69,4 @@ if __name__ == '__main__':
     print(average_value(m))
     print(collect_to_tuple(m))
     pytest.main()
+    unittest.main()
---
+++ task_03.py
@@ -0,0 +1,29 @@
+"""
+Имеется список списков
+a = [[1,2,3], [4,5,6]]
+Задание:
+сделать список словарей
+b = [{'k1': 1, 'k2': 2, 'k3': 3}, {'k1': 4, 'k2': 5, 'k3': 6}]
+*написать решение в одну строку
+"""
+import pytest
+
+
+def list_of_dictionaries(a):
+    """Список словарей"""
+    key_list = ['k1', 'k2', 'k3']
+    return f'b = {[{key_list[0]: i[0], key_list[1]: i[1], key_list[2]: i[2]} for i in a]}'
+
+
+a = [[1, 2, 3], [4, 5, 6]]
+
+
+def test_list_of_dictionaries():
+    """Pytest"""
+    expected_result = "b = [{'k1': 1, 'k2': 2, 'k3': 3}, {'k1': 4, 'k2': 5, 'k3': 6}]"
+    assert list_of_dictionaries([[1, 2, 3], [4, 5, 6]]) == expected_result
+
+
+if __name__ == '__main__':
+    print(list_of_dictionaries(a))
+    pytest.main()
